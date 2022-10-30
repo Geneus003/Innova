790f1765f6973393d77b0fbd9103e8796c7bfbdf
geneus003 <geneus003@gmail.com>
2022-10-30 15:07:48 +0300

    Database

--- innova/auth/models.py
+++ innova/auth/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Users(models.Model):
+    mail = models.CharField(max_length=128)
+    name = models.CharField(max_length=64)
+    surname = models.CharField(max_length=64)
+    second_name = models.CharField(max_length=64)
+    birthday = models.DateTimeField()
+    root = models.BooleanField(default=False)
+
---
+++ innova/auth/static/script.js
@@ -0,0 +1,113 @@
+const filterPlace = $('.filter').offset().top;
+const filterOffset =
+  $(window).width() - $('.filter').offset().left - $('.filter').width() - 30;
+const filterWidth = $(window).width() - $('.filter').offset().left;
+// console.log($('.filter').offset().left);
+console.log($(window).width());
+
+$(window).click(function (e) {
+  if (e.target.classList[1] != 'input') {
+    $('#d1').fadeOut('fast');
+    $('#d2').fadeOut('fast');
+  }
+});
+
+$(window).scroll(function () {
+  const scrollHeight = $(this).scrollTop();
+
+  // console.log(filterOffset);
+  console.log(filterWidth);
+
+  if ($(window).width() > 768) {
+    if (filterPlace < scrollHeight) {
+      $('.filter').addClass('filter-active');
+      $('.filter').css({ 'padding-right': filterOffset, width: filterWidth });
+    } else {
+      $('.filter').removeClass('filter-active');
+      $('.filter').css({ 'padding-right': '6%', width: '33.33333%' });
+    }
+  }
+});
+
+$('.filter__need').click(function (e) {
+  $('#d1').fadeToggle('fast');
+  let position = $('.filter__need').offset();
+  let top = position.top + $('.filter__need').innerHeight();
+  let left = position.left;
+
+  $('#d2').fadeOut('fast');
+
+  $('#d1').css({
+    top: top,
+    left: left,
+    width: $('.filter__need').innerWidth(),
+  });
+});
+
+$('.filter__specialization').click(function (e) {
+  $('#d2').fadeToggle('fast');
+  let position = $('.filter__specialization').offset();
+  let top = position.top + $('.filter__specialization').innerHeight();
+  let left = position.left;
+  $('#d1').fadeOut('fast');
+
+  $('#d2').css({
+    top: top,
+    left: left,
+    width: $('.filter__need').innerWidth(),
+  });
+});
+
+$('.dropdown__item').click(function (e) {
+  $('#d1').fadeOut('fast');
+  $('#d2').fadeOut('fast');
+  let tar = $(e.target);
+
+  // console.log(tar.parent().attr('id'));
+  // console.log(tar.text());
+  if (tar.parent().attr('id') == 'd1') {
+    $('.filter__need').val(tar.text());
+  } else {
+    $('.filter__specialization').val(tar.text());
+  }
+});
+
+$('.filter__need').keyup(function (e) {
+  let tar = $(e.target);
+  let value = tar.val();
+  $('#d1').fadeIn('fast');
+
+  $.each($('#d1 > .dropdown__item'), function (indexInArray, valueOfElement) {
+    if ($(valueOfElement).text().indexOf(value) == -1) {
+      $(valueOfElement).hide();
+    } else {
+      $(valueOfElement).show();
+    }
+  });
+});
+
+$('.filter__specialization').keyup(function (e) {
+  let tar = $(e.target);
+  let value = tar.val();
+  $('#d2').fadeIn('fast');
+
+  $.each($('#d2 > .dropdown__item'), function (indexInArray, valueOfElement) {
+    if ($(valueOfElement).text().indexOf(value) == -1) {
+      $(valueOfElement).hide();
+    } else {
+      $(valueOfElement).show();
+    }
+  });
+});
+
+//reset button
+
+$('.filter__reset').click(function (e) {
+  $('.filter__specialization').val('');
+  $('.filter__need').val('');
+});
+
+$('.mobile-filter').click(function (e) {
+  // $('.filter').toggle();
+  $('.filter').toggleClass('hidden');
+});
