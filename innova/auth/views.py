2e9e86f54b985c0ac2a58c6372b33ae59a1cf11e
FetVil <filimonov.vetal@gmail.com>
2022-10-30 11:59:48 +0300

    Commit message.

--- innova/auth/views.py
+++ innova/auth/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
 
 def auth_function(request):
-    return HttpResponse("Страница приложения")
+    template = loader.get_template('auth/login.html')
+    context = None
+    return HttpResponse(template.render(context, request))
