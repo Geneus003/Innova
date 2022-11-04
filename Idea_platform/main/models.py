3b121823b64557233bf1bce6c7ea77e0e5fc0504
geneus003 <geneus003@gmail.com>
2022-11-04 12:29:41 +0300

    Database update (added tags, cats, updated ideas)

--- Idea_platform/main/models.py
+++ Idea_platform/main/models.py
@@ -21,12 +21,30 @@ class Commands(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=1024)
 
+    
+class Categories(models.Model):
+    name = models.CharField(max_length=255)
+
 
 class Ideas(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, default=1)
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=1024)
     release_date = models.DateField(auto_now_add=True)
-    command_id = models.ForeignKey(Commands, default=None, on_delete=models.CASCADE)
+    moderated = models.IntegerField(default=2)
+    published = models.BooleanField(default=True)
 
-    
+
+class Tags(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class IdeasTags(models.Model):
+    idea_id = models.ForeignKey(Ideas, on_delete=models.CASCADE)
+    tag_id = models.ForeignKey(Tags, on_delete=models.CASCADE)
+
+
+class AuthorIdeas(models.Model):
+    author_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    idea_id = models.ForeignKey(Ideas, on_delete=models.CASCADE)
